7bada66c7c0c696d74e81dfe0c99219b4f5841f2
Neakail David <neakail.david@gmail.com>
2026-09-03 16:27:59 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String fixed = "";
+
+        for(int i = 0; i < s.length(); i++){
+            int curChar = s.charAt(i);
+            if(curChar >= 65 && curChar <= 90){
+                curChar = (curChar + 32);
+            }
+
+            if(curChar >= 48 && curChar <= 57){
+                fixed += (char)curChar;
+                continue;
+            } else if (curChar >= 97 && curChar <= 122){
+                fixed += (char)curChar;                
+            }
+        }
+        int i = 0;
+        int j = fixed.length() - 1;
+        while(i <= j){
+            int charOne = fixed.charAt(i);
+            int charTwo = fixed.charAt(j);
+            if(charOne != charTwo){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
